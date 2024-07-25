8aa9049496ace67e42d0a27387c6edb21b0101a2
kvbqq <kvbqqq@gmail.com>
2024-07-25 23:30:48 +0200

    feat: about page created

--- app/o-akademii/page.tsx
+++ app/o-akademii/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
 export default function About() {
+  const isDesktop = useMediaQuery("(min-width: 1000px)");
+
   return (
     <main className="overflow-hidden">
-      <h1>O Akademii</h1>
+      <section>
+        <article className={`${isDesktop ? "" : ""}`}>
+          <h1></h1>
+          <div>
+            <p></p>
+          </div>
+        </article>
+        <article></article>
+        <article></article>
+        <article></article>
+      </section>
     </main>
   );
 }
