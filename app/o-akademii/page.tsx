1c7acb72e7c188106d448ba9da1b614377e2110d
kvbqq <kvbqqq@gmail.com>
2024-07-28 22:41:45 +0200

    feat: about new article

--- app/o-akademii/page.tsx
+++ app/o-akademii/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { ReactNode } from "react";
 import Image from "next/image";
 
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 
-const PillarCard = () => {
-  return <div></div>;
+import { MdOutlineSportsVolleyball } from "react-icons/md";
+import { IoMusicalNotesOutline } from "react-icons/io5";
+import { RiPsychotherapyLine } from "react-icons/ri";
+
+interface PillarCardProps {
+  icon: ReactNode;
+  title: string;
+  text: string;
+}
+
+const PillarCard: React.FC<PillarCardProps> = ({ icon, title, text }) => {
+  return (
+    <div className={"px-6 py-8 rounded-2xl border border-gray-100 shadow-lg"}>
+      {icon}
+      <h1 className={"mt-5 mb-2 font-semibold"}>{title}</h1>
+      <p className={"text-sm"}>{text}</p>
+    </div>
+  );
 };
 
 export default function About() {
@@ -13,8 +30,8 @@ export default function About() {
 
   return (
     <main className="overflow-hidden">
-      <section className={"pb-28 flex flex-col items-center bg-white"}>
-        <article className={`pt-40 ${isDesktop ? "w-[55rem]" : ""}`}>
+      <section className={"pb-28 flex flex-col gap-24 items-center bg-white"}>
+        <article className={`pt-36 ${isDesktop ? "w-[55rem]" : ""}`}>
           <h1 className={"py-10 text-2xl font-semibold"}>O akademii</h1>
           <div className={`w-full flex items-end ${isDesktop ? "" : ""}`}>
             <p className={`${isDesktop ? "pr-24" : ""}`}>
@@ -39,7 +56,7 @@ export default function About() {
             />
           </div>
         </article>
-        <article className={`pt-24 ${isDesktop ? "w-[55rem]" : ""}`}>
+        <article className={`${isDesktop ? "w-[55rem]" : ""}`}>
           <div className={"flex flex-col items-center space-y-5"}>
             <h1 className={"text-2xl font-semibold text-custom-gold"}>
               Filary treningu w akademii
@@ -50,9 +67,47 @@ export default function About() {
               kształtowanie umiejętności <br /> i charakteru naszych zawodników.
             </p>
           </div>
+          <div className={"mt-8 flex justify-between gap-4"}>
+            <PillarCard
+              icon={<MdOutlineSportsVolleyball size={48} color="#C69A0D" />}
+              title="Treningi siatkarskie"
+              text="Regularne, intensywne zajęcia siatkarskie rozwijające technikę gry, umiejętności zespołowe, kondycję fizyczną i wytrzymałość. Program treningów jest dostosowany do różnych poziomów zaawansowania."
+            />
+            <PillarCard
+              icon={<IoMusicalNotesOutline size={48} color="#C69A0D" />}
+              title="Treningi rytmiki i koordynacji"
+              text="Zajęcia skoncentrowane na poprawie rytmiki, koordynacji ruchowej oraz zwinności. Treningi wspierają ogólny rozwój fizyczny uczestników, pomagając w lepszym radzeniu sobie z wyzwaniami sportowymi."
+            />
+            <PillarCard
+              icon={<RiPsychotherapyLine size={48} color="#C69A0D" />}
+              title="Spotkania z psychologiem sportowym"
+              text="Dbamy nie tylko o rozwój sportowy, ale także o zdrowie i bezpieczeństwo naszych podopiecznych, dlatego regularnie organizujemy spotkania z ekspertami psychologii sportu, które są dostępne również dla rodziców."
+            />
+          </div>
         </article>
-        <article></article>
-        <article></article>
+        <article className={`${isDesktop ? "w-[55rem]" : ""}`}>
+          <h1 className={"py-10 text-2xl font-semibold"}>
+            Profesjonalne obiekty sportowe
+          </h1>
+          <p>
+            Nasze zajęcia odbywają się w nowoczesnych obiektach sportowych,
+            które zapewniają komfort i bezpieczeństwo podczas treningów.
+            Posiadamy przestronne hale z profesjonalnym wyposażeniem, co pozwala
+            na efektywne i przyjemne prowadzenie zajęć sportowych.
+          </p>
+        </article>
+        <article className={`${isDesktop ? "w-[55rem]" : ""}`}>
+          <h1 className={"py-10 text-2xl font-semibold"}>Regionalizm</h1>
+          <p>
+            Sosnowiec to nie tylko nasza lokalizacja, to serce naszej akademii,
+            źródło pasji i duma każdego zawodnika Płomienia Milowice. Miasto
+            bogate w historię, tradycję i talent stanowi niezwykłe tło dla
+            naszych działań. Jako akademia z głębokimi korzeniami w tej okolicy,
+            promujemy jej wartości i wspieramy lokalną społeczność. Sosnowiec to
+            nasz dom, gdzie rozwijamy się nie tylko jako zawodnicy, ale także
+            jako ludzie.
+          </p>
+        </article>
       </section>
     </main>
   );
