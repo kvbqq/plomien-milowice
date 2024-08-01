db72f50275c8c9744e5c5b5631034771b47d60eb
kvbqq <kvbqqq@gmail.com>
2024-08-01 21:46:16 +0200

    feat: coaches and patronclub desktop pages created

--- app/klub-patronow/page.tsx
+++ app/klub-patronow/page.tsx
@@ -1,7 +1,54 @@
+"use client";
+
+import Image from "next/image";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
 export default function PatronClub() {
+  const isDesktop = useMediaQuery("(min-width: 1000px)");
+
   return (
     <main>
-      <h1>Klub 100 patronów</h1>
+      <section className={"pt-44 pb-28 flex flex-col items-center bg-white"}>
+        <article
+          className={`${
+            isDesktop ? "w-[55rem]" : ""
+          } flex flex-col items-center gap-14`}
+        >
+          <Image
+            src={"https://plomienmilowice.pl/plomien-milowice-logo.webp"}
+            alt="logo"
+            width={200}
+            height={200}
+          />
+          <h1 className={"text-3xl font-semibold"}>
+            Klub 100 Patronów Akademii
+          </h1>
+          <p className={"text-center text-lg font-semibold"}>
+            Zapraszamy do wsparcia Akademii Siatkówki Płomień Milowice poprzez
+            dołączenie do Klubu 100 Patronów Akademii. Chcemy być wiodącą
+            siatkarską akademią, w której każdy młody sportowiec znajdzie
+            wsparcie i inspirację do rozwijania swoich pasji i umiejętności.
+            Twoja dobrowolna comiesięczna wpłata w postaci symbolicznej cegiełki
+            na rozwój Akademii pomoże nam w zakupie sprzętu sportowego
+            potrzebnego do rozwoju naszych młodych sportowców, a także w
+            pokryciu kosztów wyjazdów na mecze i turnieje. <br /> <br />{" "}
+            Dołączając do Klubu 100 Patronów, stajesz się częścią społeczności,
+            która wspiera rozwój młodych talentów i promuje zdrowy styl życia.
+            Każda wpłata ma ogromne znaczenie i jest bezpośrednim inwestowaniem
+            w przyszłość naszej młodzieży. Państwa wsparcie będzie miało ogromne
+            znaczenie i duży wpływ na rozwój Akademii. Będziemy wdzięczni za
+            okazaną pomoc i wsparcie naszego nowego projektu. <br /> <br />{" "}
+            Rozpalmy razem płomień sukcesu! Dziękujemy za Twoje wsparcie.
+          </p>
+          <p className={"px-14 py-6 bg-custom-gold rounded-xl"}>
+            Numer konta do wpłat:{" "}
+            <span className={"font-semibold"}>
+              61 1020 2498 0000 8402 0819 1944
+            </span>
+          </p>
+        </article>
+      </section>
     </main>
   );
 }
--- app/nasi-trenerzy/page.tsx
+++ app/nasi-trenerzy/page.tsx
@@ -1,9 +1,87 @@
+"use client";
+
+import { ReactNode } from "react";
+import Image from "next/image";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
+interface CoachCardProps {
+  image: string;
+  name: string;
+  text: string;
+  isEven: boolean;
+}
+
+const CoachCard: React.FC<CoachCardProps> = ({ image, name, text, isEven }) => {
+  const isDesktop = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <article
+      className={`flex items-center relative ${isDesktop ? "w-[55rem]" : ""}`}
+    >
+      <div
+        className={`flex ${
+          isEven ? "flex-row-reverse pr-8" : "pl-8"
+        } jsutify-between py-8 z-10 bg-white bg-opacity-30 backdrop-blur-xl rounded-2xl border border-gray-300 shadow-lg`}
+      >
+        <h1 className="w-3/5 text-justify">{text}</h1>
+        <div className={"w-2/5 flex relative justify-center"}>
+          <Image
+            src={image}
+            alt={name}
+            width={3307}
+            height={4724}
+            style={{ height: "350px", width: "auto" }}
+          />
+          <h1
+            className={
+              "absolute bottom-0 bg-nav-bg-start py-4 px-10 text-white font-medium rounded-xl"
+            }
+          >
+            {name}
+          </h1>
+        </div>
+      </div>
+      <div
+        className={`h-[70%] aspect-square absolute rounded-full bg-custom-gold ${
+          isEven ? "-right-[9rem]" : "-left-[9rem]"
+        }`}
+      ></div>
+    </article>
+  );
+};
+
 export default function CoachesInfo() {
   return (
     <main>
       <section
-        className={"py-28 flex flex-col gap-24 items-center bg-white"}
-      ></section>
+        className={"pt-44 pb-28 flex flex-col gap-10 items-center bg-white"}
+      >
+        <CoachCard
+          image="https://plomienmilowice.pl/agata-kopczyk.jpg"
+          name="Agata Kopczyk"
+          text="Założycielka Akademii Siatkówki Płomień Milowice. Jako była siatkarka, wielokrotnie zdobywała Mistrzostwo Polski. Razem z Płomieniem Milowice zdobyła Wicemistrzostwo oraz brązowy medal Mistrzostw Polski. Wielokrotna reprezentantka Polski. Po zakończeniu kariery sportowej poświęciła się trenowaniu młodzieży. Razem ze swoimi drużynami wielokrotnie osiągała liczne sukcesy na wielu turniejach ogólnopolskich, w tym Mistrzostwach Polski. Jej podopieczne regularnie występują w Tauron Lidze i reprezentują kraj na arenie międzynarodowej. Trenerka Agata z pasją i profesjonalizmem rozwija umiejętności sportowe młodzieży, budując ich charakter i promując zasady fair play. Tworzy otwartą, wspierającą atmosferę, stale doskonaląc swoje metody treningowe."
+          isEven={false}
+        />
+        <CoachCard
+          image="https://plomienmilowice.pl/agnieszka-legien.jpg"
+          name="Agnieszka Legień"
+          text="Trenerka Agnieszka Legień ukończyła Akademię Wychowania Fizycznego w Katowicach z przygotowaniem pedagogicznym. Jest wychowanką Płomienia Milowice, gdzie w drużynach młodzieżowych zdobywała medale Mistrzostw Polski. Od lat z pasją szkoli młodzież w mini siatkówce, osiągając liczne sukcesy. Jej podejście do dzieci charakteryzuje się zaangażowaniem i troską, tworząc inspirujące środowisko dla młodych sportowców. Dzięki swojemu poświęceniu i profesjonalizmowi, trenerka Agnieszka skutecznie motywuje młodych zawodników do osiągania pełnego potencjału."
+          isEven={true}
+        />
+        <CoachCard
+          image="https://plomienmilowice.pl/paulina-baltrukiewicz.jpg"
+          name="Paulina Bałtrukiewicz"
+          text="Trenerka Paulina Bałtrukiewicz w naszej akademii specjalizuje się w poprawie koordynacji i motoryki zawodników. Ukończyła Akademię Wychowania Fizycznego w Katowicach, zdobywając specjalizacje z wychowania fizycznego i gimnastyki korekcyjnej. Od wielu lat pracuje jako trenerka przygotowania motorycznego Zdobyła doświadczenie jako II trenerka piłki siatkowej Kadry Śląska Młodziczek rocznika 2009 oraz jako obecna trenerka przygotowania motorycznego Kadry Polski U15 Młodziczek. Jej nowoczesne metody treningowe pomagają zawodnikom poprawić ich koordynację i kondycję. Trenerka Paulina znana jest z indywidualnego podejścia do każdego podopiecznego, co pozwala na dostosowanie treningów do ich potrzeb i możliwości."
+          isEven={false}
+        />
+        <CoachCard
+          image="https://plomienmilowice.pl/nadia-peszko.jpg"
+          name="Nadia Peszko"
+          text="Trenerka Nadia Peszko jest absolwentką psychologii na Uniwersytecie SWPS ze specjalizacją psychologii sportu i aktywności fizycznej oraz studiów podyplomowych z psychologii sportu na Poznańskim AWF. Posiada bogate doświadczenie w pracy indywidualnej, jako członek sztabów szkoleniowych i dydaktyk na uczelniach wyższych. Współpracuje ze sportowcami na różnym etapie kariery i poziomie zaawansowania. W przeszłości była reprezentantką klubów Polskiej Ligi Siatkówki, młodzieżową reprezentantką Polski i medalistką Mistrzostw Polski. W Akademii Siatkówki Płomień Milowice trenerka Nadia pracuje zarówno z zawodnikami, jak i ich rodzicami, wspierając rozwój mentalny młodych sportowców."
+          isEven={true}
+        />
+      </section>
     </main>
   );
 }
